afb5a64a3ed1579c95634cdc0e57fd4e14a8eb05
Sandro-Gabunia <gabuniasandro99@gmail.com>
2024-03-09 22:55:00 +0400

    search function added

--- app/src/main/java/com/example/final_project/data/remote/service/search/ProductSearchService.kt
+++ app/src/main/java/com/example/final_project/data/remote/service/search/ProductSearchService.kt
@@ -3,15 +3,8 @@ package com.example.final_project.data.remote.service.search
 import com.example.final_project.data.remote.model.search.ProductsDto
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
-//interface ProductSearchService {
-//    @GET("https://dummyjson.com/products/search?q={search}")
-//    suspend fun getProduct(
-//        @Path("search") search: String
-//    ): Response<ProductsDto>
-//}
 
 interface ProductSearchService {
     @GET("https://dummyjson.com/products/search")
