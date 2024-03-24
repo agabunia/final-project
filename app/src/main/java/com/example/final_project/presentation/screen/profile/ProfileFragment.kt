4ce0ed0436d5cee5172575f96691eeaf8e0d867f
Sandro-Gabunia <gabuniasandro99@gmail.com>
2024-03-24 16:27:42 +0400

    select image from gallery added

--- app/src/main/java/com/example/final_project/presentation/screen/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/final_project/presentation/screen/profile/ProfileFragment.kt
@@ -1,5 +1,13 @@
 package com.example.final_project.presentation.screen.profile
 
+import android.Manifest
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Build
+import android.provider.MediaStore
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -17,6 +25,18 @@ import kotlinx.coroutines.launch
 class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate) {
     private val viewModel: ProfileViewModel by viewModels()
 
+    private val permissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {}
+
+    private val galleryLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        val galleryUri = it
+        try {
+            binding.ivProfileImage.setImageURI(galleryUri)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
     override fun bind() {
         (activity as? MainActivity)?.showBottomNavigationBar()
     }
@@ -28,6 +48,11 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBind
 
         binding.layoutTerms.setOnClickListener {
             viewModel.onEvent(ProfileEvent.Terms)
+        }
+
+        binding.btnEditImage.setOnClickListener {
+            requestPermission()
+            galleryLauncher.launch("image/*")
         }
     }
 
@@ -54,6 +79,17 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBind
 
     private fun navigateToTerms() {
         findNavController().navigate(ProfileFragmentDirections.actionProfileFragmentToTermsFragment())
+    }
+
+    private fun requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            permissionLauncher.launch(
+                arrayOf(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            )
+        }
     }
 
 }
